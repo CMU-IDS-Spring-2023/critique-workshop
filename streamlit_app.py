93da5c0990fbd4fcb61ab997ba2baae9047410cb
Venkatesh Sivaraman <venkatesh.sivaraman.98@gmail.com>
2022-03-17 14:56:18 -0400

    Fix datatypes

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ ORDERS = {
 
 @st.cache
 def load_data():
-    return pd.read_csv("pulse_survey_sampled.csv")
+    return pd.read_csv("pulse_survey_sampled.csv", dtype={"age": int, "hhld_num_persons": int, "food_spending_unprepared": int, "food_spending_prepared": int})
 
 @st.cache
 def get_slice_membership(df, genders=None, educations=None, races=None, age_range=None, marital_status=None, income=None):
